53cae5584ab697edb4f8f11b30ce0f2ddac43dec
PaulChomik <75439631+PaulChomik@users.noreply.github.com>
2022-04-27 07:31:21 -0300

    segundo commit con 2 ejercicios

---
+++ 0_impresion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#esto se llama shebang el bin/bash se ocupa para indicar que interprete usar
+echo "holanda"
+# este scrip se ejecuta escribiendo "bash impresion.sh"
+
---
+++ 1_imp_con_argumentos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "holanda $1"
+#el primer argumento que le pasemos sera uado en  lugar de $1
+# pasando "bash imp_con_argumentos nombre"
+#imprimirá "holanda nombre"
